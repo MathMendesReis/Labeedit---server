76f6c191500dba0b6ce73f0d1c4e090c1382bdef
Matheus Mendes <matheusmendesreis@outlook.com>
2024-11-12 15:50:13 -0300

    initial commit

---
+++ src/main/java/com/mendes/Labeedit/LabeeditApplication.java
@@ -0,0 +1,13 @@
+package com.mendes.Labeedit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabeeditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabeeditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mendes/Labeedit/LabeeditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendes.Labeedit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabeeditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
