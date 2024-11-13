1b120c15a3c24bebe0ee21db2f9020382e35a5aa
Matheus Mendes <matheusmendesreis@outlook.com>
2024-11-13 20:59:34 -0300

    new: create new entitie AppUser

---
+++ src/main/java/com/mendes/Labeedit/modules/user/entities/AppUser.java
@@ -0,0 +1,45 @@
+package com.mendes.Labeedit.modules.user.entities;
+
+import com.mendes.Labeedit.utils.AbstractEntitie;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "app_users")
+public class AppUser extends AbstractEntitie{
+  @Column(nullable = false)
+  private String name;
+  @Column(unique = true, nullable = false)
+  private String email;
+
+  private String password;
+
+  private UserRole role;
+
+  public AppUser(String name,String email,String password){
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+  public AppUser(){
+    
+  }
+
+  public String getEmail(){
+    return this.email;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+  public UserRole getRole(){
+    return this.role;
+  }
+  public String getPassword() {
+    return password;
+}
+
+  
+}
